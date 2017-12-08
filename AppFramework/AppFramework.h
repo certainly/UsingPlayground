427b91036fd471f4dfdc44bdf4bafb678f830a76
certainly <j1o2b3@gmail.com>
2017-12-08 16:00:46 +0800

    + ignore

---
+++ AppFramework/AppFramework.h
@@ -0,0 +1,19 @@
+//
+//  AppFramework.h
+//  AppFramework
+//
+//  Created by certainly on 2017/12/8.
+//  Copyright © 2017年 certainly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppFramework.
+FOUNDATION_EXPORT double AppFrameworkVersionNumber;
+
+//! Project version string for AppFramework.
+FOUNDATION_EXPORT const unsigned char AppFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFramework/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+#platform :ios, '9.0'
+use_frameworks!
+
+pod 'Cheers'
+
+target 'UsingPlayground'
+target 'AppFramework'
